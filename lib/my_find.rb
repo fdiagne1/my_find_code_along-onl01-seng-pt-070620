e9a09099260ec9fbf6e576cc08254938e76ed3f1
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-16 22:17:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,7 @@ def my_find(collection)
   i = 0
   
   while i < collection.length
-  yield(collection[i])
-    return collection[i]
+    return collection[i] if yield(collection[i])
    end
   i += 1
   end
